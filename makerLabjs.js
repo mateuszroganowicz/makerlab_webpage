b8e04a0df65265ad20cad0b33e28d2f9b6330a34
mateuszroganowicz <matiroganowicz@gmail.com>
2020-06-24 13:51:09 +0200

    mobile app pictures added

--- makerLabjs.js
+++ makerLabjs.js
@@ -1,28 +1,66 @@
-var slideIndex = 1;
-    var slides = document.getElementsByClassName("mySlides");
-    for (i = 0; i < slides.length; i++) {
-        slides[i].style.display = "none";
-    }
-    slides[slideIndex-1].style.display = "block";
-    showSlides(slideIndex);
+var slideIndexTechnician = 1;
+var slideIndexStudent = 1;
+var slideIndexMobile = 1;
 
-    function plusSlides(n) {
-    showSlides(slideIndex += n);
-    }
 
-    function currentSlide(n) {
-    showSlides(slideIndex = n);
-    }
 
-    function showSlides(n) {
+function plusSlidesTechnician(n) {
+    showSlidesTechnician(slideIndexTechnician += n);
+}
+
+function currentSlideTechnician(n) {
+    showSlidesTechnician(slideIndexTechnician = n);
+}
+
+function showSlidesTechnician(n) {
     var i;
-    var slides = document.getElementsByClassName("mySlides");
-    if (n > slides.length) {slideIndex = 1}
-    if (n < 1) {slideIndex = slides.length}
-    for (i = 0; i < slides.length; i++) {
-        slides[i].style.display = "none";
+    var slidesTechnician = document.getElementsByClassName("slidesTechnician");
+    if (n > slidesTechnician.length) {slideIndexTechnician = 1}
+    if (n < 1) {slideIndexTechnician = slidesTechnician.length}
+    for (i = 0; i < slidesTechnician.length; i++) {
+        slidesTechnician[i].style.display = "none";
     }
-    
-    slides[slideIndex-1].style.display = "block";
+
+    slidesTechnician[slideIndexTechnician-1].style.display = "block";
+}
+
+function plusSlidesStudent(m) {
+    showSlidesStudent(slideIndexStudent += m);
+}
+
+function currentSlideStudent(m) {
+    showSlidesStudent(slideIndexStudent = m);
+}
+
+function showSlidesStudent(m) {
+    var j;
+    var slidesStudent = document.getElementsByClassName("slidesStudent");
+
+    if (m > slidesStudent.length) {slideIndexStudent = 1}
+    if (m < 1) {slideIndexStudent = slidesStudent.length}
+    for (j = 0; j < slidesStudent.length; j++) {
+        slidesStudent[j].style.display = "none";
     }
+    slidesStudent[slideIndexStudent-1].style.display = "block";
+}
+
+function plusSlidesMobile(m) {
+    showSlidesMobile(slideIndexMobile += m);
+}
+
+function currentSlideStudent(m) {
+    showSlidesMobile(slideIndexMobile = m);
+}
+
+function showSlidesMobile(m) {
+    var j;
+    var slidesMobile = document.getElementsByClassName("slidesMobile");
+
+    if (m > slidesMobile.length) {slideIndexMobile = 1}
+    if (m < 1) {slideIndexMobile = slidesMobile.length}
+    for (j = 0; j < slidesMobile.length; j++) {
+        slidesMobile[j].style.display = "none";
+    }
+    slidesMobile[slideIndexMobile-1].style.display = "block";
+}
 
